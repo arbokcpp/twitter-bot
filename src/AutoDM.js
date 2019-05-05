dce524d869a2d5e8b34b1cc99be0e14469e91f84
arbokcpp <45389780+arbokcpp@users.noreply.github.com>
2019-05-05 15:21:05 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,8 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
-  // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+  return `Hi ${name} Welcome to my twitter,\n you can also find me on instagram: https://www.instagram.com/arbowk/?hl=it \n and twitch: https://www.twitch.tv/arbowk \n Happy ${dayName}` ^^; // your message
 };
 
 module.exports = AutoDM;
